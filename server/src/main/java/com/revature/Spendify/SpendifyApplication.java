76f9f32f40c1bd9235be984cb43a3f8aa76084ec
hhuyn019 <hhuyn019@ucr.edu>
2024-01-05 08:47:19 -0800

    added server and client directorties, spring boot project

---
+++ server/src/main/java/com/revature/Spendify/SpendifyApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Spendify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendifyApplication.class, args);
+	}
+
+}
