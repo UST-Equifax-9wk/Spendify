065970dbc8ed6caf9ec5457e56bf15157061f402
BeepingJohnson <jdfranco@asu.edu>
2024-01-09 08:52:54 -0700

    front end registering

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DistributorRegisterComponent } from './distributor-register/distributor-register.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, DistributorRegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { DistributorRegisterComponent } from './distributor-register/distributor-register.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 export const routes: Routes = [
-    {path: "navbar", component: NavbarComponent}
+    {path: "navbar", component: NavbarComponent},
+    {path: "distributor-register", component: DistributorRegisterComponent}
+
 ];
