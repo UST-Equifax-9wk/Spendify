97cd5375f658ead38542167542aa6a13a2e9cf23
BeepingJohnson <jdfranco@asu.edu>
2024-01-09 15:11:58 -0700

    stubbing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { DistributorRegisterComponent } from './distributor-register/distributor-register.component';
+import { ManageInventoryComponent } from './manage-inventory/manage-inventory.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
 
 export const routes: Routes = [
     {path: "navbar", component: NavbarComponent},
     {path: "distributor-register", component: DistributorRegisterComponent},
-    {path: "register-user", component: RegisterUserComponent}
+    {path: "register-user", component: RegisterUserComponent},
+    {path: "manage-inventory", component: ManageInventoryComponent},
+
 ];
 
